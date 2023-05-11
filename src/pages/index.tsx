bc6263136808db77a4e3a30a4eccea021c801dc4
sa4mi <111993418+saaaaashimi@users.noreply.github.com>
2023-05-12 01:06:36 +0900

    Introduce Vercel postgres
    Created table forrelation between a wallet address and an Shopify access token

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,19 @@
-import { ConnectButton } from '@rainbow-me/rainbowkit';
-import Head from 'next/head'
-import { useAccount } from 'wagmi'
+import { ConnectButton } from "@rainbow-me/rainbowkit";
+import Head from "next/head";
+import { AccessTokenWalletAddress } from "@prisma/client";
+import { useAccount } from "wagmi";
+
+import { useEffect, useState } from "react";
 
 const fetchShop = async (accessToken: string) => {
   try {
-    const response = await fetch('/api/store/getByAccessToken', { method: 'POST',  headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ accessToken }), });
+    const response = await fetch("/api/store/getByAccessToken", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ accessToken }),
+    });
     if (response.ok) {
       const shopData = await response.json();
       console.log(shopData);
@@ -17,10 +23,51 @@ const fetchShop = async (accessToken: string) => {
   } catch (error) {
     console.error(`Error fetching store data: ${error}`);
   }
-}
+};
 
 export default function Home() {
-  const { address } = useAccount()
+  const [accessTokenWalletAddress, setAccessTokenWalletAddress] =
+    useState<AccessTokenWalletAddress>();
+  const { address } = useAccount();
+
+  useEffect(() => {
+    if (address === undefined) return;
+
+    (async () => {
+      try {
+        const response = await fetch(
+          "/api/accessTokenWalletAddress/getByWalletAddress",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ walletAddress: address }),
+          }
+        );
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          if (data === null) {
+            console.log(
+              "Not accessTokenWalletAddress found for the address: ",
+              address
+            );
+            return;
+          }
+
+          setAccessTokenWalletAddress(data);
+        } else {
+          console.log(
+            `Error fetching accessTokenWalletAddress: ${response.status}`
+          );
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    })();
+  }, [address]);
+
   return (
     <>
       <Head>
@@ -31,17 +78,55 @@ export default function Home() {
       </Head>
       <main>
         <ConnectButton />
-        <p>Wallet address: {address}</p>
-        <form onSubmit={(event) => {
-          event.preventDefault();
-          const accessToken = (event.target as HTMLFormElement).accessToken.value;
-          fetchShop(accessToken);
-        }}>
-          <label htmlFor="accessToken">Access token</label>
-          <input type="password" id="accessToken" name="accessToken" />
-          <button type="submit">Fetch store info</button>
-        </form>
+        {address !== undefined && accessTokenWalletAddress === undefined && (
+          <form
+            onSubmit={async (event) => {
+              event.preventDefault();
+              const accessToken = (event.target as HTMLFormElement).accessToken
+                .value;
+
+              try {
+                const response = await fetch(
+                  "/api/accessTokenWalletAddress/create",
+                  {
+                    method: "POST",
+                    headers: {
+                      "Content-Type": "application/json",
+                    },
+                    body: JSON.stringify({
+                      accessToken,
+                      walletAddress: address,
+                    }),
+                  }
+                );
+                if (response.ok) {
+                  const data = await response.json();
+                  console.log('Created new accessTokenWalletAddress');
+                  setAccessTokenWalletAddress(data);
+                } else {
+                  console.log(`Error creatiing new accessTokenWalletAddress: ${response.status}`)
+                }
+              } catch (error) {
+                console.error(`Error creating new accessTokenWalletAddress: ${error}`);
+              }
+            }}
+          >
+            <label htmlFor="accessToken">Access token</label>
+            <input type="password" id="accessToken" name="accessToken" />
+            <button type="submit">Save access token</button>
+          </form>
+        )}
+        {accessTokenWalletAddress !== undefined && (
+          <button
+            type="button"
+            onClick={async () => {
+              fetchShop(accessTokenWalletAddress.accessToken);
+            }}
+          >
+            Fetch store info
+          </button>
+        )}
       </main>
     </>
-  )
+  );
 }
