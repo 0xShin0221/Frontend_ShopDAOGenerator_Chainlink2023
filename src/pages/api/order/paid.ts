1b5d39372c629d3672b4e3a136699e142bb9c42b
sintaro0221 <sintaronettt@gmail.com>
2023-06-01 17:52:23 +0900

    tst: write distribute function of sc

--- src/pages/api/order/paid.ts
+++ src/pages/api/order/paid.ts
@@ -1,12 +1,20 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import { salesDistributorContractClient } from "@/utils/salesDistributorContractClient";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+const mockData = {
+  nftAddress: "0x1234567890123456789012345678901234567890",
+  orderId: "testOrderId",
+  totalSale: 500,
+  totalProfit: 100,
+};
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  console.log(`Webhook: Order payment`)
-  console.log(`method: ${req.method}`)
+  console.log(`Webhook: Order payment`);
+  console.log(`method: ${req.method}`);
   if (req.method !== "POST") {
     res.statusCode = 405;
     res.setHeader("Allow", "POST");
@@ -14,6 +22,27 @@ export default async function handler(
     return;
   }
 
-  console.log('Order payment:', req.body)
-  res.status(200).json(req.body)
+  if (
+    !mockData.nftAddress ||
+    !mockData.orderId ||
+    !mockData.totalSale ||
+    !mockData.totalProfit
+  ) {
+    res.status(400).json({ error: "Missing necessary order data" });
+    return;
+  }
+
+  try {
+    const txHash = await salesDistributorContractClient(
+      mockData.nftAddress,
+      mockData.orderId,
+      mockData.totalSale,
+      mockData.totalProfit
+    );
+
+    // Send a response with the transaction hash
+    res.status(200).json({ txHash });
+  } catch (error) {
+    res.status(500).json({ error: "Something went wrong." });
+  }
 }
