b0ff92fa4352d4fe8c392e5c1a904f238ced490e
sa4mi <111993418+sa4mi@users.noreply.github.com>
2023-06-04 18:19:15 +0900

    chore: FIx Loading contract spinner to display on only mint-profit-nft page

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,21 +1,18 @@
-import { ReactNode, memo, useContext } from "react";
-import { Box, Container, VStack } from "@chakra-ui/react";
+import { ReactNode, memo } from "react";
 
 import { _abiDaoFactory } from "../../../abi";
-import { Header, Loading } from "@/components";
+import { Header } from "@/components";
 import { Footer } from "../Footer";
-import { NftContractContext } from "@/contexts";
 
 type Props = {
   children: ReactNode;
 };
 
 export const Layout = memo(({ children }: Props): JSX.Element => {
-  const store = useContext(NftContractContext)
   return (
     <>
       <Header />
-      {store.isLoading ? <Loading /> : children}
+      {children}
       <Footer />
     </>
   );
--- src/pages/mint-profit-right-nft.tsx
+++ src/pages/mint-profit-right-nft.tsx
@@ -5,7 +5,7 @@ import { Flex, VStack, Button, Box, Text } from '@chakra-ui/react'
 import { useAddress } from '@thirdweb-dev/react'
 import { useContext } from 'react'
 import { getChain } from '@/helper'
-import { Fade, NftImagesSlideShow } from '@/components'
+import { Fade, Loading, NftImagesSlideShow } from '@/components'
 import { useConnectWallet } from '@/hooks/thirdweb'
 
 const chain = getChain()
@@ -29,41 +29,43 @@ const MintProfitRightNft = () => {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Flex
-        maxW={'8xl'}
-        justifyContent="center"
-        h="100%"
-        alignItems="center"
-        mx="auto"
-      >
-        <Fade>
-          <VStack spacing={6} padding="1rem">
-            <Box width="240px" height="240px">
-              <NftImagesSlideShow />
-            </Box>
+      {store.isLoading ? <Loading /> : (
+        <Flex
+          maxW={'8xl'}
+          justifyContent="center"
+          h="100%"
+          alignItems="center"
+          mx="auto"
+        >
+          <Fade>
+            <VStack spacing={6} padding="1rem">
+              <Box width="240px" height="240px">
+                <NftImagesSlideShow />
+              </Box>
 
-            <div>
-            {address ? (
-              <Button onClick={mint} disabled={store.isClaiming}>
-                {store.isClaiming
-                  ? 'claiming...'
-                  : `MINT (${store.claimPrice} ETH)`}
-              </Button>
-            ) : (
-              <Button onClick={connectWallet}>
-                <Text fontSize="xs">Connect Wallet</Text>
-              </Button>
-            )}
-              <Text pt={2} fontSize="xs" textAlign={'center'}>
-                {store.claimedSupply} / {store.totalSupply}
-              </Text>
-              <Text pt={2} fontSize="xs" textAlign={'center'}>
-                {chain.name}
-              </Text>
-            </div>
-          </VStack>
-        </Fade>
-      </Flex>
+              <div>
+                {address ? (
+                  <Button onClick={mint} disabled={store.isClaiming}>
+                    {store.isClaiming
+                      ? 'claiming...'
+                      : `MINT (${store.claimPrice} ETH)`}
+                  </Button>
+                ) : (
+                  <Button onClick={connectWallet}>
+                    <Text fontSize="xs">Connect Wallet</Text>
+                  </Button>
+                )}
+                <Text pt={2} fontSize="xs" textAlign={'center'}>
+                  {store.claimedSupply} / {store.totalSupply}
+                </Text>
+                <Text pt={2} fontSize="xs" textAlign={'center'}>
+                  {chain.name}
+                </Text>
+              </div>
+            </VStack>
+          </Fade>
+        </Flex>
+      )}
     </>
   )
 }
