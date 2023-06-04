e5095460917d4af7a518bf17e87428a541af5b62
sa4mi <111993418+sa4mi@users.noreply.github.com>
2023-06-04 22:12:07 +0900

    debug: console.log

--- src/helper/getChain/getChain.ts
+++ src/helper/getChain/getChain.ts
@@ -1,5 +1,8 @@
 import { Mumbai, Polygon } from "@thirdweb-dev/chains";
 
-export const getChain = () => process.env.NODE_ENV === "production"
-? Polygon // Matic
-: Mumbai; // Mumbai
+export const getChain = () => {
+    console.log('NODE_ENV', process.env.NODE_ENV)
+    return process.env.NODE_ENV === "production"
+        ? Polygon // Matic
+        : Mumbai; // Mumbai
+}
